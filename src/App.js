668129d4e9256a02b0d4bae81dc3b8b92ce86af2
alinak <alinak@radware.com>
2020-02-25 16:01:08 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Tooltip } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tooltip placement="topLeft" title="prompt text">
+    <span>Tooltip will show on mouse enter.</span>
+  </Tooltip>
   );
 }
 
